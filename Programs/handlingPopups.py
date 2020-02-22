45dd735caab7922517f9ee06244d323db9ff2faf
Aditya Raj <aditya.x510@gmail.com>
2020-02-22 14:57:15 +0530

    init

--- Programs/handlingPopups.py
+++ Programs/handlingPopups.py
@@ -13,9 +13,19 @@ driver = webdriver.Chrome("../Drivers/x32/chromedriver.exe")
 driver.maximize_window()
 driver.implicitly_wait(3)
 driver.get("https://www.seleniumeasy.com/test/javascript-alert-box-demo.html")
-clickBtn = driver.find_element_by_css_selector("button[onclick='myAlertFunction\(\)']")
+# clickBtn = driver.find_element_by_css_selector("button[onclick='myAlertFunction\(\)']")
+#
+# clickBtn.click()
+# alertJS = driver.switch_to.alert
+# print(alertJS.text)
+# alertJS.accept()
 
-clickBtn.click()
-alertJS = driver.switch_to.alert()
-print(alertJS.text)
-alertJS.accept()
+
+clickBtnCancel = driver.find_element_by_css_selector("#easycont > div > div.col-md-6.text-left >"
+                                                     " div:nth-child(5) > div.panel-body > button")
+clickBtnCancel.click()
+# cancel
+alertCancel = driver.switch_to.alert
+print(alertCancel.text)
+time.sleep(5)
+alertCancel.dismiss()
