a623f1b570e63e203d5b8b479a6456bb40ec4446
Aditya Raj <aditya.x510@gmail.com>
2020-02-22 10:20:30 +0530

    init

--- Programs/launchChromeBrowser.py
+++ Programs/launchChromeBrowser.py
@@ -1,5 +1,7 @@
 from selenium import webdriver as wb
 
+
+# relative and absolute path for giving the path of browser drivers
 driver = wb.Chrome()
 # launching the url using get
-driver.get("")
+driver.get("https://www.https://www.seleniumeasy.com/")
